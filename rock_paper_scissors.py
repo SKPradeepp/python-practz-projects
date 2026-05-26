55fe4bc7459947ae2b246f849067a0d708980f72
SKPradeepp <skpiitmember2008@gmail.com>
2026-05-26 14:37:00 +0530

    rock paper scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,15 @@
+import random
+choices = ["rock", "paper", "scissors"]
+computer=random.choice(choices)
+user=input("Enter your choice (rock, paper, scissors): ").lower()
+if user == computer:
+    print("It's a tie!")
+elif (
+    (user == "rock" and computer == "scissors") or
+    (user == "paper" and computer == "rock") or
+    (user == "scissors" and computer == "paper")
+):
+    print("You win!")
+else:
+    print("Computer wins!")
+print("Computer chose:", computer)
