3e63ce88235e5a516d0823e907af070d9754cced
Pradeepp <skpiitmember2008@gmail.com>
2026-05-27 13:02:01 +0530

    Modify marks and enhance plot with titles and labels
    Updated marks data and added plot titles and labels.

--- graph.py
+++ graph.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 
-marks = [90, 75, 82]
+marks = [85, 90, 78]
 
 plt.plot(marks)
+
+plt.title("Student Marks")
+
+plt.xlabel("Students")
+
+plt.ylabel("Marks")
 
 plt.show()
